bc53a2ba86ed25fc1570cebe5a02e4e44c2aba87
Timelord <Timelord@F4>
2015-06-06 16:54:01 +0300

    Triangular commit!

---
+++ src/Triangular.java
@@ -0,0 +1,15 @@
+
+public class Triangular {
+	public static void main(String[] args) {
+		
+		int i,k;
+		System.out.println("+");
+				for ( i = 1; i<10; i++){
+					System.out.print("+");
+					for (k = i; k>0;k--){
+						System.out.print(" ");}
+					System.out.println("+");
+				}
+				System.out.println("++++++++++++");
+			}
+}
